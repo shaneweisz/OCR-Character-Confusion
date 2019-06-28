0eae1ebb13eb5889a0c37c1755f7e84641cfc247
Nic Kroon <nickroon@Nics-MacBook-Pro-6.local>
2019-06-28 11:49:42 +0200

    Working implementation of if you supply your own truth and read text files.

--- Makefile
+++ Makefile
@@ -1,9 +1,8 @@
 default:
 	./generate_conf_matrix.sh
 
-# Ensure the supplied text files are in the same directory as the Makefile
-confusion_matrix: # EXAMPLE USAGE make confusion_matrix t=truth.txt r=read.txt
-	./conf_matrix_from_text.sh $(t) $(r)
+conf_matrix: # EXAMPLE USAGE make confusion_matrix t=truth.txt r=read.txt
+	./conf_matrix_from_text.sh $t $r
 
 clean:
 	rm confusion_matrix/confusion_matrix.pkl
--- conf_matrix_from_text.sh
+++ conf_matrix_from_text.sh
@@ -1,4 +1,8 @@
+# these commands place the supplied truth and read text files into the directories needed by our scripts
+make clean # remove the current pickle
 
+cp $t char_data_generation/random_chars.txt
+cp $r char_data_generation/recognized_chars.txt
 
 pypy3 confusion_matrix/file_comparison.py # align truth and read
 python3 confusion_matrix/generate_confusion_matrix.py # calculate and update confusion matrix
