61830b39838e7dacc233e427460e3827e2af99e5
Shane Weisz <shaneweisz@gmail.com>
2019-07-01 10:55:02 +0200

    Moved weighted lev and made minor changes.

--- conf_matrix_from_text.sh
+++ conf_matrix_from_text.sh
@@ -1,6 +1,6 @@
-# these commands place the supplied truth and read text files into the directories needed by our scripts
 make clean # remove the current pickle
 
+# these commands place the supplied truth and read text files into the directories needed by our scripts
 cp $t char_data_generation/random_chars.txt
 cp $r char_data_generation/recognized_chars.txt
 
--- weighted_lev.py
+++ weighted_lev.py
@@ -1,8 +1,8 @@
-from file_comparison import get_absolute_path
 import numpy as np
 import pandas as pd
 import weighted_levenshtein as wl
 import sys
+import os
 
 
 def normalise(df):
@@ -29,9 +29,24 @@ def get_subsitution_costs(df):
     return sub_costs
 
 
+def get_absolute_path(relative_path):
+    '''
+    Parameters:
+        relative_path --> a string with the path to a file relative to the
+                          current directory
+                          Example: "../char_data_generation/random_chars.txt"
+
+    Returns:
+        A string with the absolute file path to a given file
+    '''
+    dirname = os.path.dirname(__file__)
+    filename = os.path.join(dirname, relative_path)
+    return filename
+
+
 def main():
-    df = pd.read_pickle(get_absolute_path('../confusion_matrix') +
-                        '/confusion_matrix.pkl')
+    df = pd.read_pickle(get_absolute_path('confusion_matrix') +
+                        '/confusion_matrix_base.pkl')  # use the base pickle
     df = df.drop('other', axis=1)  # drop the ' ' column
     df = normalise(df)
 
